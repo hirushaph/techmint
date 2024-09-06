b5283d008c4808ab5c3981e48524e094c43621a0
Hirusha Prabhanath <hirushaph@gmail.com>
2024-09-06 22:10:30 +0530

    Add demo producs

--- src/features/shop/components/CategoryFilter.tsx
+++ src/features/shop/components/CategoryFilter.tsx
@@ -1,9 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import CheckBox from '../../../components/CheckBox';
 import SidebarWidget from './SidebarWidget';
+import { RootStateType } from '../../../redux/store';
 
 const category = ['Budget', 'Midrange', 'Flagship'];
 
 function CategoryFilter() {
+  const dispatch = useDispatch();
+  const selectedCategories = useSelector(
+    (state: RootStateType) => state.filter.selectedCategory
+  );
+
   return (
     <SidebarWidget title='Category'>
       <ul className='flex flex-col gap-1'>
--- src/redux/filterSlice.ts
+++ src/redux/filterSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 type FilterType = {
   priceRange: [number, number] | null;
+  selectedCategory: string[];
 };
 
 const initialState: FilterType = {
   priceRange: null,
+  selectedCategory: [],
 };
 
 const filterSlice = createSlice({
@@ -15,9 +17,20 @@ const filterSlice = createSlice({
     setPriceRange: (state, action: PayloadAction<[number, number]>) => {
       state.priceRange = action.payload;
     },
+    toggleCategory: (state, action: PayloadAction<string>) => {
+      const category = action.payload;
+
+      if (state.selectedCategory.includes(category)) {
+        state.selectedCategory = state.selectedCategory.filter(
+          (item) => item !== category
+        );
+      } else {
+        state.selectedCategory.push(category);
+      }
+    },
   },
 });
 
-export const { setPriceRange } = filterSlice.actions;
+export const { setPriceRange, toggleCategory } = filterSlice.actions;
 
 export default filterSlice.reducer;
